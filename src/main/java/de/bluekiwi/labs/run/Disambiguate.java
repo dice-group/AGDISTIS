82bd3d60f26eb44ceb43c36173e81ce9f750def4
Ricardo Usbeck <r.usbeck@linuxdeployer.unister.lan>
2013-05-27 13:20:30 +0200

    runnable disambiguation

---
+++ src/main/java/de/bluekiwi/labs/run/Disambiguate.java
@@ -0,0 +1,43 @@
+package de.bluekiwi.labs.run;
+
+import java.io.IOException;
+
+import com.unister.semweb.topicmodeling.utils.doc.Document;
+import com.unister.semweb.topicmodeling.utils.doc.DocumentText;
+import com.unister.semweb.topicmodeling.utils.doc.ner.NamedEntitiesInText;
+import com.unister.semweb.topicmodeling.utils.doc.ner.NamedEntityInText;
+
+import de.bluekiwi.labs.algorithm.NEDAlgo_HITS;
+
+public class Disambiguate {
+
+    public static void main(String[] args) throws IOException {
+        String languageTag = "de"; // de,en
+        String dataDirectory = "/data/r.usbeck";
+        int maxDepth = 2;
+        double threshholdTrigram = 0.835;
+
+        // DISAMBIGUATION ALGORITHM
+        NEDAlgo_HITS algo = new NEDAlgo_HITS(1, languageTag, dataDirectory);
+        // NEDAlgo_wihtoutGraphTechniques algo = new NEDAlgo_wihtoutGraphTechniques(corpus.getNumberOfDocuments());
+
+        Document document = new Document();
+        NamedEntitiesInText nes = new NamedEntitiesInText(new NamedEntityInText(37, 15, "Golf_Geo/Mexiko"),
+                new NamedEntityInText(28, 7, "Golf_Car/VI"),
+                new NamedEntityInText(9, 9, "Jaguar_Car/RX"));
+        DocumentText text = new DocumentText("Der neue Jaguar RX ist kein Golf VI. Golf von Mexiko");
+
+        document.addProperty(text);
+        document.addProperty(nes);
+
+        algo.run(document, threshholdTrigram, maxDepth);
+        // algo.run(document, threshholdTrigram); --> used for algo without graph techniques
+        NamedEntitiesInText namedEntities = document.getProperty(NamedEntitiesInText.class);
+        for (NamedEntityInText namedEntity : namedEntities) {
+            String disambiguatedURL = algo.findResult(namedEntity);
+            System.out.println(disambiguatedURL);
+        }
+
+        algo.close();
+    }
+}
