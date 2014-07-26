29519cf3851f11da4c44e689af80c8abc9be9cba
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-07-26 19:25:30 +0200

    resolved compiler issue

--- src/main/java/org/aksw/agdistis/ner/StanfordNER.java
+++ src/main/java/org/aksw/agdistis/ner/StanfordNER.java
@@ -1,25 +1,25 @@
-package org.aksw.agdistis.ner;
-
-import java.io.IOException;
-
-import edu.stanford.nlp.ie.AbstractSequenceClassifier;
-import edu.stanford.nlp.ie.crf.CRFClassifier;
-import edu.stanford.nlp.ling.CoreLabel;
-
-public class StanfordNER {
-
-    public static void main(String[] args) throws IOException {
+//package org.aksw.agdistis.ner;
+//
+//import java.io.IOException;
+//
+//import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+//import edu.stanford.nlp.ie.crf.CRFClassifier;
+//import edu.stanford.nlp.ling.CoreLabel;
+//
+//public class StanfordNER {
+//
+//    public static void main(String[] args) throws IOException {
         // String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
-        String serializedClassifier = "classifiers/dewac_175m_600.crf.ser.gz";
+//        String serializedClassifier = "classifiers/dewac_175m_600.crf.ser.gz";
         // String text = " Chancelor Angela Merkel will go meet Barack Obama President of the U.S. at Berlin";
-        String text = "Bundeskanzlerin Angela Merkel trifft sich mit US-Präsident Obama bei Siemens in Berlin";
-        System.out.println(runNER(text, serializedClassifier));
-    }
+//        String text = "Bundeskanzlerin Angela Merkel trifft sich mit US-Präsident Obama bei Siemens in Berlin";
+//        System.out.println(runNER(text, serializedClassifier));
+//    }
 
-    public static String runNER(String text, String serializedClassifier) throws IOException {
-        AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier
-                .getClassifierNoExceptions(serializedClassifier);
-        return (classifier.classifyWithInlineXML(text));
-    }
+  //  public static String runNER(String text, String serializedClassifier) throws IOException {
+    //    AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier
+      //          .getClassifierNoExceptions(serializedClassifier);
+        //return (classifier.classifyWithInlineXML(text));
+   // }
 
-}
+//}
