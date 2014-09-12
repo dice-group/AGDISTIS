11d89f1a81c5cd497fcc2b470b3c36a11229ff64
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-09-12 12:25:25 +0200

    Deleted database functionality and judgement agreement  * deleted data base java classes  * deleted judgement agreement class  * deleted leftovers from AIDA and the uber topicmodelling
    GOAL: Future experiments using AGDISTIS should be performed on another project

--- src/main/java/org/aksw/agdistis/db/ConfigurableRowMapper.java
+++
@@ -1,9 +0,0 @@
-package org.aksw.agdistis.db;
-
-import org.springframework.jdbc.core.RowMapper;
-
-public interface ConfigurableRowMapper<T> extends RowMapper<T> {
-
-    public static final int NOT_IN_RESULT_SET = -1;
-
-}
