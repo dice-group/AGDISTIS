ff154e363bc9896a52a35a39a4f8f5c51cf615dd
Tim Ermilov <yamalight@gmail.com>
2017-01-16 17:25:28 +0100

    Add simple curl e2e test for docker image before releasing it

---
+++ test-docker.sh
@@ -0,0 +1,16 @@
+# define correct result
+RESULT="[{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Leipzig_University\",\"offset\":21,\"namedEntity\":\"University of Leipzig\",\"start\":5},{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Barack_Obama\",\"offset\":12,\"namedEntity\":\"Barack Obama\",\"start\":30}]\c"
+echo $RESULT > expected.txt
+
+# get response
+curl -s --data-urlencode "text='The <entity>University of Leipzig</entity> in <entity>Barack Obama</entity>.'" -d type='agdistis' http://localhost:8080/AGDISTIS > response.txt
+
+# check
+if cmp --silent expected.txt response.txt;
+then
+  echo "OK, correct response!";
+  exit 0;
+else
+  echo "Error! Response doesn't match!";
+  exit 1;
+fi
