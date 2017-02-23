5e498c79a47bd33b70d2ac146c7a7c1a62c11a14
Diego Moussallem <diegomoussallem@gmail.com>
2017-02-23 17:07:56 +0100

    just a remedy for a while.
    Probably there is a best solution

--- test-docker.sh
+++ test-docker.sh
@@ -1,9 +1,9 @@
 # define correct result
-RESULT="[{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Leipzig_University\",\"offset\":21,\"namedEntity\":\"University of Leipzig\",\"start\":5},{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Barack_Obama\",\"offset\":12,\"namedEntity\":\"Barack Obama\",\"start\":30}]\c"
-echo $RESULT > expected.txt
+RESULT="[{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Leipzig_University\",\"offset\":21,\"namedEntity\":\"University of Leipzig\",\"start\":5},{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Barack_Obama\",\"offset\":12,\"namedEntity\":\"Barack Obama\",\"start\":30}]"
+echo $RESULT > sed 's/ *$//' expected.txt
 
 # get response
-curl -s --data-urlencode "text='The <entity>University of Leipzig</entity> in <entity>Barack Obama</entity>.'" -d type='agdistis' http://http://139.18.2.164:8080/AGDISTIS > response.txt
+curl -s --data-urlencode "text='The <entity>University of Leipzig</entity> in <entity>Barack Obama</entity>.'" -d type='agdistis' http://139.18.2.164:4445/AGDISTIS > sed 's/ *$//' response.txt
 
 # log response
 echo "Got response:"
