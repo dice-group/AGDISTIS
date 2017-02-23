16640d6967ee3ed3d2fe0a506dbb163f8bb340fc
Diego Moussallem <diegomoussallem@gmail.com>
2017-02-23 19:34:42 +0100

    Update test-docker.sh

--- test-docker.sh
+++ test-docker.sh
@@ -1,9 +1,9 @@
 # define correct result
-RESULT="[{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Leipzig_University\",\"offset\":21,\"namedEntity\":\"University of Leipzig\",\"start\":5},{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Barack_Obama\",\"offset\":12,\"namedEntity\":\"Barack Obama\",\"start\":30}]"
-echo $RESULT > sed 's/ *$//' expected.txt
+RESULT="[{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Leipzig_University\",\"offset\":21,\"namedEntity\":\"University of Leipzig\",\"start\":5},{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Barack_Obama\",\"offset\":12,\"namedEntity\":\"Barack Obama\",\"start\":30}]";
+echo $RESULT > expected.txt
 
 # get response
-curl -s --data-urlencode "text='The <entity>University of Leipzig</entity> in <entity>Barack Obama</entity>.'" -d type='agdistis' http://localhost:4445/AGDISTIS > sed 's/ *$//' response.txt
+curl -s --data-urlencode "text='The <entity>University of Leipzig</entity> in <entity>Barack Obama</entity>.'" -d type='agdistis' http://localhost:4445/AGDISTIS > +"\n" response.txt;
 
 # log response
 echo "Got response:"
