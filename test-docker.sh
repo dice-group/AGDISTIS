823ae98588f82e395eea3e505aaded03c8bd53ed
Diego Moussallem <diegomoussallem@gmail.com>
2017-02-24 09:18:46 +0100

    trying to make it run.

--- test-docker.sh
+++ test-docker.sh
@@ -1,9 +1,9 @@
 # define correct result
-RESULT="[{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Leipzig_University\",\"offset\":21,\"namedEntity\":\"University of Leipzig\",\"start\":5},{\"disambiguatedURL\":\"http:\/\/dbpedia.org\/resource\/Barack_Obama\",\"offset\":12,\"namedEntity\":\"Barack Obama\",\"start\":30}]";
+RESULT='[{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Angela_Merkel","offset":13,"namedEntity":"Angela Merkel","start":1},{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Barack_Obama","offset":12,"namedEntity":"Barack Obama","start":19},{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Berlin","offset":6,"namedEntity":"Berlin","start":39}]'
 echo $RESULT > expected.txt
 
 # get response
-curl -s --data-urlencode "text='The <entity>University of Leipzig</entity> in <entity>Barack Obama</entity>.'" -d type='agdistis' http://localhost:4445/AGDISTIS > response.txt
+curl -s --data-urlencode "text='<entity>Angela Merkel</entity> and <entity>Barack Obama</entity> are in <entity>Berlin</entity>.'" -d type='agdistis' http://localhost:8080/AGDISTIS > response.txt
 echo "" >> response.txt
 
 # log response
