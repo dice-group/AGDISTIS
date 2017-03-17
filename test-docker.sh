472df45d29dd5ea028cfb474144947d7d86568e5
Tim Ermilov <yamalight@gmail.com>
2017-03-17 15:06:46 +0100

    Fix test docker script

--- test-docker.sh
+++ test-docker.sh
@@ -1,15 +1,15 @@
 # define correct result
-RESULT='[{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Angela_Merkel","offset":13,"namedEntity":"Angela Merkel","start":1},{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Barack_Obama","offset":12,"namedEntity":"Barack Obama","start":19},{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Berlin","offset":6,"namedEntity":"Berlin","start":39}]'
+RESULT='[{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Angela_Merkel","offset":13,"namedEntity":"Angela Merkel","start":1},{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Barack_Obama","offset":12,"namedEntity":"Barack Obama","start":19},{"disambiguatedURL":"http:\/\/dbpedia.org\/resource\/Berlin","offset":6,"namedEntity":"Berlin","start":39}]\c'
 echo $RESULT > expected.txt
 
 # get response
 curl -s --data-urlencode "text='<entity>Angela Merkel</entity> and <entity>Barack Obama</entity> are in <entity>Berlin</entity>.'" -d type='agdistis' http://localhost:8080/AGDISTIS > response.txt
-echo "" >> response.txt
 
 # log response
 echo "Got response:"
+echo "\n"
 cat response.txt
-echo ""
+echo "\n\n"
 
 # check
 if cmp -s expected.txt response.txt;
